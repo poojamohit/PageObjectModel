c47daebac466b06e2e96b00405ab5d4710375d2e
poojamohit <33201423+poojamohit@users.noreply.github.com>
2018-09-01 22:35:18 +0530

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "/home/ubuntu/jenkin-slave/workspace/sel-test/webapp-addresbbokproject/chromedriver");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
